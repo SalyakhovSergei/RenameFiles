dcb5c3911cfaa08e563e04adb1df96dce3a5c485
Sergey <salyakhovsergei@gmail.com>
2021-09-10 12:59:26 +1000

    Initial commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Анатолий\OneDrive\Desktop\ess";
+            var files = Directory.GetFiles(path);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].Contains("OneRepublic"))
+                {
+                    Console.WriteLine(files[i]);
+                    File.Move(files[i], files[i].Remove(39, 14));
+                }
+            }
+            
+            
+        }
+    }
+}
